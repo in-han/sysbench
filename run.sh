447d20a15630de893d9fc3a6b431e408d4ed1b6b
yin.han <yin.han@gmail.com>
2023-09-07 09:44:47 +0800

    x

--- run.sh
+++ run.sh
@@ -10,9 +10,7 @@ sysbench ./src/lua/oltp_read_only_v2.lua help
 
 OPTIONS="--mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=app_onebox --mysql-password= --mysql-db=db_onebox --skip_trx=on --auto_inc=off --create_secondary=off --table_size=100 --db-ps-mode=disable "
 
-OPTIONS="--mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=app_onebox --mysql-password= --mysql-db=db_onebox --skip_trx=on --auto_inc=off --create_secondary=off --table_size=100 "
+OPTIONS="--mysql-host=127.0.0.1 --mysql-port=3306 --mysql-user=app_onebox --mysql-password= --mysql-db=db_onebox --skip_trx=on --auto_inc=off --create_secondary=off --table_size=100 --threads=1 --tables=8 --validate=on"
 $SYSBENCH ./src/lua/oltp_read_only_v2.lua prepare $(echo $OPTIONS)
 
-$SYSBENCH ./src/lua/oltp_read_only_v2.lua run $(echo $OPTIONS)
-
-
+$SYSBENCH ./src/lua/oltp_read_only_v2.lua run $(echo $OPTIONS) --threads=8
